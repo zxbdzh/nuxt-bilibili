34de844cb9d460b1e33a42e4884a4a8ce00d3a00
zxbdzh <1205039851@qq.com>
2025-04-28 21:31:50 +0800

    项目完成

--- app.vue
+++ app.vue
@@ -1,22 +1,13 @@
-<template>
-  <div>
-    <!--  Nuxt路由  -->
-    <NuxtLink to="/">首页</NuxtLink>
-    <br />
-    <NuxtLink to="/video">视频</NuxtLink>
-    <NuxtPage/>
-  </div>
-  <van-button type="primary">主要按钮</van-button>
-  <van-button type="info">信息按钮</van-button>
-</template>
 <script setup lang="ts">
-useSeoMeta({
-  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
-  description: '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。'
-})
 </script>
-<style>
-:root {
-  --van-primary-color: #FB7299 !important;
-}
+
+<template>
+  <nuxt-page :keepalive="{ max: 10 }" />
+</template>
+
+<style lang="scss">
+// 基础样式
+@use './assets/styles/base.scss';
+// 字体图标
+@use './assets/styles/iconfont.scss';
 </style>
